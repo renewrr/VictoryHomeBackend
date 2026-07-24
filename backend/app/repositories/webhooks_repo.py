c4ea436287c95754226b9310ed18a1029510cdcd
renewrr <46344868+renewrr@users.noreply.github.com>
2026-07-24 17:06:10 +0800

    Update webhooks_repo.py
    Main message building, physical condition debug.

--- backend/app/repositories/webhooks_repo.py
+++ backend/app/repositories/webhooks_repo.py
@@ -10,14 +10,55 @@ from sqlalchemy.orm import with_loader_criteria, selectinload, noload
 
 
 class WebhookRepository:
-    pass
 
     @staticmethod
     def post_webhook_message(
         response_id: str, timestamp_str: str, answers: dict[str, str]
     ):
         timestamp = datetime.datetime.fromisoformat(timestamp_str)
-        employee_name = answers.get("交班者 Người giao ca ")
-        print("Original:", timestamp_str, answers)
-        print("Transformed:", employee_name, timestamp, flush=True)
-        pass
+        employee_name = answers.get("交班者 Người giao ca ", "").strip("")
+        if not employee_name:
+            return False
+        try:
+            employee = (
+                db_manager.session.query(models.Employee)
+                .where(models.Employee.name == employee_name)
+                .one()
+            )
+        except:
+            employee = models.Employee(name=employee_name)
+            db_manager.session.add(employee)
+            db_manager.session.commit()
+        shift_str = answers.get("班別 Ca làm việc ", "").strip()
+        if "白班" in shift_str:
+            shift_str = "day"
+        elif "小夜班" in shift_str:
+            shift_str = "evening"
+        elif "大夜班" in shift_str:
+            shift_str = "night"
+        else:
+            shift_str = "day"
+        shift = (
+            db_manager.session.query(models.Shifts)
+            .where(models.Shifts.name == shift_str)
+            .one()
+        )
+        main_msg = models.HandoverMessage(
+            timestamp=timestamp,
+            creator_id=employee.ID,
+            shift=shift.name,
+            google_form_response_id=response_id,
+        )
+        # db_manager.session.add(main_msg)
+        # db_manager.session.commit()
+        handle_physcon(answers, main_msg)
+
+
+def handle_physcon(answers: dict[str, str], main_msg: models.HandoverMessage):
+    physcon_str = answers.get(
+        "服務對象身體狀況\n  (Tình trạng sức khỏe và khám bệnh)  ", ""
+    )
+    print(physcon_str, flush=True)
+    if not physcon_str or '無 Không có gì bất thường' in physcon_str:
+        return
+    pass

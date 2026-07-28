539559d07bd9e84c6bbf60f47ba04e28b579fe37
renewrr <46344868+renewrr@users.noreply.github.com>
2026-07-28 16:45:13 +0800

    Update auth_repo.py
    Debug

--- backend/app/repositories/auth_repo.py
+++ backend/app/repositories/auth_repo.py
@@ -41,6 +41,7 @@ def get_active_user(user_id: int) -> EmployeeCache:
 
 
 def remove_user_cache(user_id: int):
+    print(f"Flushing cache for {user_id}, in cache? {user_id in USERCACHE}", flush=True)
     if user_id in USERCACHE:
         USERCACHE.pop(user_id)
 
